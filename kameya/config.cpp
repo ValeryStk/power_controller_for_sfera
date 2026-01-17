1b12a12f8ace7e179be543d6f33536e794b35ff9
Valery <tor-tech@mail.ru>
2026-01-17 18:56:07 +0300

    add json keys

--- kameya/config.cpp
+++ kameya/config.cpp
@@ -1,7 +1,22 @@
 #include "config.h"
 
 namespace global {
-const QString json_lamps_file_name       = "ir_lamps.json";
-const QString path_to_log_dir            = "/_logs";
-const QString relative_path_to_log_file  = "/_logs/logic.log";
+const QString json_lamps_file_name        = "ir_lamps.json";
+const QString path_to_log_dir             = "/_logs";
+const QString relative_path_to_log_file   = "/_logs/logic.log";
+
+const char kJsonKeyLampsArray[]           = "lamps";
+const char kJsonKeyLampsCoordsObject[]    = "lamps_item_coords";
+const char kJsonKeyPowerSupply1_Object[]  = "ps1";
+const char kJsonKeyPowerSupply2_Object[]  = "ps2";
+const char kJsonKeyPowerSupply3_Object[]  = "ps3";
+
+const char kJsonKeyIsUnlockKeyFlag[]      = "is_unlock";
+const char kJsonKeyName[]                 = "name";
+const char kJsonKeyX[]                    = "x";
+const char kJsonKeyY[]                    = "y";
+const char kJsonKeyColor[]                = "color";
+const char kJsonKeyIp[]                   = "ip";
+const char kJsonKeyMaxCurrent[]           = "max_current";
+const char kJsonKeyMaxOut[]               = "out";
 }
--- kameya/config.h
+++ kameya/config.h
@@ -5,10 +5,28 @@
 
 namespace global{
 
+// file names and dirs
 extern const QString json_lamps_file_name;
 extern const QString path_to_log_dir;
 extern const QString relative_path_to_log_file;
 
-}
+// root objects
+extern const char kJsonKeyLampsArray[];
+extern const char kJsonKeyLampsCoordsObject[];
+extern const char kJsonKeyPowerSupply1_Object[];
+extern const char kJsonKeyPowerSupply2_Object[];
+extern const char kJsonKeyPowerSupply3_Object[];
+
+// common json keys
+extern const char kJsonKeyIsUnlockKeyFlag[];
+extern const char kJsonKeyName[];
+extern const char kJsonKeyX[];
+extern const char kJsonKeyY[];
+extern const char kJsonKeyColor[];
+extern const char kJsonKeyIp[];
+extern const char kJsonKeyMaxCurrent[];
+extern const char kJsonKeyMaxOut[];
+
+}// end namespace global
 
 #endif // CONFIG_H
